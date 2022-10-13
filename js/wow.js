1039459729ebef39d488820bf2471f0c8f9b2e72
Irina <valkiria0000@bk.ru>
2022-10-13 23:47:31 +0300

    add project

---
+++ js/wow.js
@@ -0,0 +1,8 @@
+wow = new WOW({
+  boxClass: "wow", // default
+  animateClass: "animated", // default
+  offset: 20, // default
+  mobile: true, // default
+  live: true, // default
+});
+wow.init();
